6116a5ce4c3e01bc2451eac0cb3ca8703879ab08
Md.Al kafee <sojeebahmed.sa@gmail.com>
2022-09-29 09:41:16 +0600

    Question section added

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -22,26 +22,52 @@ const Body = () => {
 
     return (
         <div className="body-container">
-            
+
             <div className="exercise-card-container">
-                
+
                 {
-                    
+
                     exercises.map(exercise => <Exercise
                         key={exercise.id}
                         exercise={exercise}
                         handleClick={handleClick}
                     ></Exercise>)
 
+
                 }
-                
+
             </div>
             <div className="profile-container  text-center p-4 rounded">
                 <Cart profile={profile}></Cart>
             </div>
-        </div>
-    )
+            <div className=' mx-auto my-5 ms-5 border border-dark rounded border-2 p-3'>
+                <h1>Questions</h1>
+                <h4>1. how does react works ?</h4>
+                <p>Answer: React is a declarative, efficient, and flexible JavaScript library for building user interfaces. 'V' denotes the view in MVC. ReactJS is an open-source, component-based front end library responsible only for the view layer of the application. It is maintained by Facebook.</p>
+                <h4>2. Difference between props and state.</h4>
+                <p>Answer: <br />
+                    PROPS:- <br />
+                    -The Data is passed from one component to another. <br />
+                    -It is Immutable (cannot be modified). <br />
+                    -Props can be used with state and functional components. <br />
+                    -Props are read-only. <br />
+                    STATE:- <br />
+                    -The Data is passed within the component only. <br />
+                    -	It is Mutable ( can be modified). <br />
+                    -State can be used only with the state components/class component (Before 16.0). <br />
+                    -	State is both read and write.
+                </p>
+                <h4>3. What is the use of use effect ?</h4>
+                <p>
+                    Answer: <br />
+                    The useEffect Hook allows you to perform side effects in your components.
+                    Some examples of side effects are: fetching data, directly updating the DOM, and timers.
+                    useEffect accepts two arguments. The second argument is optional.
+                    </p>
+                    </div>
+            </div>
+            )
 };
 
-export default Body;
+            export default Body;
 
--- src/components/Exercise/Exercise.js
+++ src/components/Exercise/Exercise.js
@@ -2,11 +2,10 @@ import React from 'react';
 
 const Exercise = (props) => {
     const { exercise, handleClick } = props;
-    const { id, name, age, img, time, description } = exercise;
+    const { name, age, img, time, description } = exercise;
     return (
-        
+
         <div className="">
-            
             <div className="card" style={{ width: "18rem" }}>
                 <img src={img} className=" card-img-top p-2" alt="" />
                 <div className="card-body">
@@ -15,10 +14,8 @@ const Exercise = (props) => {
                     <p className="card-text">For Age: {age}</p>
                     <p className="card-text">Time Required: {time} m</p>
                     <button onClick={() => handleClick(exercise)} className="btn btn-primary w-100">Add to list</button>
-
                 </div>
             </div>
-
         </div>
     );
 };
